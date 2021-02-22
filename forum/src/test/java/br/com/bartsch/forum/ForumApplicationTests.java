c7173fa1d0cc7a1b7eb50a3308886768f0807723
AlessandroBartsch <49681821+AlessandroBartsch@users.noreply.github.com>
2021-02-22 15:30:21 -0300

    base project

---
+++ forum/src/test/java/br/com/bartsch/forum/ForumApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bartsch.forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
