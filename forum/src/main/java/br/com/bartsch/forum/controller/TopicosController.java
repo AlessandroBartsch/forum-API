b5fadae44afde9a2a793245c0cfaf9658e86a48b
Alessandro Bartsch <alessandro.bartsch@cingo.com.br>
2021-02-24 17:10:04 -0300

    Controller do topico retorna dados do banco atraves do repository
    Realizar requisição através da url:
    http://localhost:8080/topicos?nomeCurso=<nome do curso cadastrado>
    
    removidos contrutores que foram usados para testes, para que o spring
    receba o contrutor padrao default da entidade

--- forum/src/main/java/br/com/bartsch/forum/controller/TopicosController.java
+++ forum/src/main/java/br/com/bartsch/forum/controller/TopicosController.java
@@ -1,23 +1,32 @@
 package br.com.bartsch.forum.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.bartsch.forum.controller.dto.TopicoDto;
-import br.com.bartsch.forum.modelo.Curso;
 import br.com.bartsch.forum.modelo.Topico;
+import br.com.bartsch.forum.repository.TopicoRepository;
 
 @RestController
 public class TopicosController {
 
+	@Autowired
+	private TopicoRepository topicoRepository;
+	
 	@RequestMapping("/topicos")
-	public List<TopicoDto> lista() {
-		Topico topico = new Topico("Duvida", "Duvida teste com Spring", new Curso("Spring", "Programação"));
+	public List<TopicoDto> lista(String nomeCurso) {
 		
-		return TopicoDto.converter(Arrays.asList(topico, topico, topico));
+		if(nomeCurso == null) {
+			List<Topico> topicos = topicoRepository.findAll();
+			return TopicoDto.converter(topicos);
+		} else {
+			List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
+			return TopicoDto.converter(topicos);
+		}
+		
 	}
 	
 }
