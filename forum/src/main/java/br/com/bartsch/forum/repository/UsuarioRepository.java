da22d1bc2cde7c8fae6796c4e66d4ed1cc59cf78
Alessandro Bartsch <alessandro.bartsch@cingo.com.br>
2021-03-02 13:11:29 -0300

    Adicionado controle de acesso e validacao de usuario com Spring Security

---
+++ forum/src/main/java/br/com/bartsch/forum/repository/UsuarioRepository.java
@@ -0,0 +1,13 @@
+package br.com.bartsch.forum.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.bartsch.forum.modelo.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+
+	Optional<Usuario> findByEmail(String email);
+	
+}
